159a05772e8534f86ed9bdd8832ca4cddc25f0b8
Martin Dousa <martindousa186@gmail.com>
2021-03-23 00:31:20 +0100

    documentation of number_class

--- src/mathLib/basics/number_class.py
+++ src/mathLib/basics/number_class.py
@@ -1,9 +1,23 @@
+##
+# @package number_class
+#
+
 from dataclasses import dataclass
 
+##
+# @brief Number class
+#
+# Hold result of interpreter and correctly represent it when converted to string
+#
 @dataclass
 class Number:
   value: float
 
+  ##
+  # @brief Check if value is float or int and return value according to it
+  #
+  # @return Int or float value
+  #
   def get_value(self):
     int_val = int(self.value)
     return self.value if int_val != self.value else int_val
