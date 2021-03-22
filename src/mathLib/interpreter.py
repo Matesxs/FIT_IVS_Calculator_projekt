ce9e43d136bb45c55809707aca87eea3bce318c9
Martin Dousa <martindousa186@gmail.com>
2021-03-23 00:48:35 +0100

    documentation of interpreter

--- src/mathLib/interpreter.py
+++ src/mathLib/interpreter.py
@@ -11,17 +11,45 @@ from .basics.math_functions import MathFunctions
 # @brief Interpreter class for executing functions based on input node tree
 #
 class Interpreter:
+  ##
+  # @brief Entry point for interpreting node tree
+  #
+  # Entry point for interpreting node tree \n
+  # Called recursively for whole tree
+  #
+  # @param node Node of node tree
+  # @return Interpreted number of Number class of current part of tree
+  #
   def interpret(self, node:Node) -> Number:
     func_name = f"visit_{type(node).__name__}"
     function = getattr(self, func_name, self.no_visit_method)
     return function(node)
 
+  ##
+  # @brief Default method if unknown node is passed
+  #
   def no_visit_method(self, node):
     raise RuntimeError(f"No visit_{type(node).__name__} function defined")
 
+  ##
+  # @brief Interpret basic number node
+  #
+  # Most simple building block
+  #
+  # @param node NumberNode to parse
+  # @return Number with value from node
+  #
   def visit_NumberNode(self, node:NumberNode):
     return Number(node.value)
 
+  ##
+  # @brief Interpret UnaryOperationNode
+  #
+  # Get value from node, interpret it and based on operation token choose operation
+  #
+  # @param node UnaryOperationNode to parse
+  # @return Number with value returned from math function
+  #
   def visit_UnaryOperationNode(self, node:UnaryOperationNode):
     number = self.interpret(node.value).get_value()
 
@@ -38,6 +66,14 @@ class Interpreter:
 
     raise RuntimeError(f"Unknown unary operation token: {node.operation_token}")
 
+  ##
+  # @brief Interpret BinaryOperationNode
+  #
+  # Get values from node, interpret them and based on operation token choose operation
+  #
+  # @param node BinaryOperationNode to parse
+  # @return Number with value returned from math function
+  #
   def visit_BinaryOperationNode(self, node:BinaryOperationNode):
     number1 = self.interpret(node.value1).get_value()
     number2 = self.interpret(node.value2).get_value()
