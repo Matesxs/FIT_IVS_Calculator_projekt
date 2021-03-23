96899b9987abb7547a531ebd99a5b3b042742354
Martin Dousa <martindousa186@gmail.com>
2021-03-23 04:30:29 +0100

    documentation of tokens

--- src/mathLib/basics/tokens.py
+++ src/mathLib/basics/tokens.py
@@ -1,6 +1,13 @@
+##
+# @package tokens
+#
+
 from dataclasses import dataclass
 from enum import Enum
 
+##
+# @brief Enum class for holding token type
+#
 class TokenType(Enum):
   NUMBER    =   0
   PLUS      =   1
@@ -13,13 +20,26 @@ class TokenType(Enum):
   RPAREN    =   8
   KEYWORD   =   9
 
+##
+# @brief Class for holding data of each token
+#
 @dataclass
 class Token:
   type: TokenType
   value: any = None
 
+  ##
+  # @brief Check if token matches type and value
+  #
+  # @param type TokenType for comparison
+  # @param value Value for comparison
+  # @return Bool True if they matches else False
+  #
   def matches(self, type:TokenType, value):
     return self.type == type and self.value == value
 
+  ##
+  # @brief Method for printing content of token
+  #
   def __repr__(self):
     return f"(TOKEN:{self.type.name}{f' VAL {self.value}' if self.value else ''})"
