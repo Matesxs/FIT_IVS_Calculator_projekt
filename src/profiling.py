9f53b1abbb3c92f040aada51d9461d9a5eda1435
Martin Dousa <martindousa186@gmail.com>
2021-03-23 04:32:52 +0100

    ...

--- src/profiling.py
+++ src/profiling.py
@@ -3,24 +3,24 @@ from mathLib.basics.math_functions import MathFunctions
 import random
 import sys
 
+# Rise recursion limit because we used them a lot so it doesnt break
 sys.setrecursionlimit(1_000_000)
 random.seed()
 
+# Generate input string for math lib and execute calculation
 def standart_deviation(numbers, n):
   input_string = f"(1 / ({n} - 1) * ({create_sum_string([MathFunctions.power_operation(float(number), 2) for number in numbers])} - {n} * ((1 / {n}) * {create_sum_string(numbers)})^2))√2"
   return interpret_text_input(input_string)
 
+# Create string with sum of all numbers
 def create_sum_string(numbers):
   total = "("
   for number in numbers:
     total += str(number) + "+"
   return total[:-1] + ")"
 
-def get_numbers_testing(x):
-  return [random.random() * 1000 for _ in range(x)]
-
+# Load user input, sanitize it and pass for formating for math lib
 def profile():
-  # numbers = get_numbers_testing(1_000)
   numbers = input("Input numbers for standart deviation: ")
   numbers = str(numbers).replace(" ", ";").replace("\t", ";").replace("\n", ";").replace("\r", "").replace(",", ".").split(";")
   print(standart_deviation(numbers, len(numbers)))
